259178402d368cf15734482226c1fc15e2b354f0
SHAKEEL ASLAM <130243197+shakeel15@users.noreply.github.com>
2024-03-06 20:22:08 +0400

    commitng all files from vs code

---
+++ main.py
@@ -0,0 +1,58 @@
+import random
+import os
+
+from art import logo, vs
+from game_data import data
+
+def get_random_account():
+    """Get data from random account"""
+    return random.choice(data)
+
+def format_data(account):
+    name = account["name"]
+    description = account["description"]
+    country = account["country"]
+    return f"{name}, a {description}, from {country}"
+
+def check_anwer(guess, a_followers, b_followers):
+    """Check followers againt user's huess
+    and returns True if thet got it right.
+    or False if they got it wrong."""
+    if a_followers > b_followers:
+        return guess == "a"
+    else:
+        return guess == "b"
+    
+def game():
+    print(logo)
+    score = 0
+    game_should_continue = True
+    account_a = get_random_account()
+    account_b = get_random_account()
+
+    while game_should_continue:
+        account_a = account_b
+        account_b = get_random_account()
+
+        while account_a == account_b:
+            account_b = get_random_account()
+
+        print(f"Compare A: {format_data(account_a)}.")
+        print(vs)
+        print(f"Against B: {format_data(account_b)}.")
+
+        guess = input("Who has more followers? Type 'A' or 'B':").lower()
+        a_follower_count = account_a["follower_count"]
+        b_follower_count = account_b["follower_count"]
+        is_correct = check_anwer(guess, a_follower_count, b_follower_count)
+        
+        os.system('cls')
+        print(logo)
+        if is_correct:
+            score +=1
+            print(f"You're right! Current Score: {score}.")
+        else:
+            game_should_continue = False
+            print(f"Sorry, that's wrong. Final Score: {score}.")
+
+game()
